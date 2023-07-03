e79642b4fd3442f5056da15025d45ad7ac152ba3
Choejeonghun <saa00123@naver.com>
2023-07-03 23:04:44 +0900

    feat: fetch를 활용하여 API 호출

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import "./App.css";
 import DiaryEditor from "./components/DiaryEditor";
 import DiaryList from "./components/DiaryList";
-import Lifecycle from "./components/Lifecycle";
-import LifecylceUnmount from "./components/LifecycleUnmount";
+// import Lifecycle from "./components/Lifecycle";
+// import LifecylceUnmount from "./components/LifecycleUnmount";
 
 function App() {
   const [data, setData] = useState([]);
 
   const dataId = useRef(0);
+
+  const getData = async () => {
+    const res = await fetch(
+      "https://jsonplaceholder.typicode.com/comments"
+    ).then((res) => res.json());
+
+    const initData = res.slice(0, 20).map((it) => {
+      return {
+        author: it.email,
+        content: it.body,
+        emotion: Math.floor(Math.random() * 5) + 1,
+        created_date: new Date().getTime(),
+        id: dataId.current++,
+      };
+    });
+
+    setData(initData);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   const onCreate = (author, content, emotion) => {
     const created_date = new Date().getTime();
@@ -39,8 +61,8 @@ function App() {
 
   return (
     <div>
-      <LifecylceUnmount />
-      <Lifecycle />
+      {/* <LifecylceUnmount />
+      <Lifecycle /> */}
       <DiaryEditor onCreate={onCreate} />
       <DiaryList diaryList={data} onRemove={onRemove} onEdit={onEdit} />
     </div>
