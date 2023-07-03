eed31bcf8eeb8b06cd81a2bd3887f9525ca9f5e0
Choejeonghun <saa00123@naver.com>
2023-07-03 22:09:51 +0900

    feat: 배열을 통해 데이터 입력, 삭제, 수정

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
-import DiaryEditor from "./components/DiaryEditor";
+import React, { useState, useRef } from "react";
 import "./App.css";
+import DiaryEditor from "./components/DiaryEditor";
+import DiaryList from "./components/DiaryList";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  const dataId = useRef(0);
+
+  const onCreate = (author, content, emotion) => {
+    const created_date = new Date().getTime();
+    const newItem = {
+      author,
+      content,
+      emotion,
+      created_date,
+      id: dataId.current,
+    };
+    dataId.current += 1;
+    setData([newItem, ...data]);
+  };
+
+  const onRemove = (targetId) => {
+    const newDiaryList = data.filter((it) => it.id !== targetId);
+    setData(newDiaryList);
+    console.log(`${targetId}가 삭제되었습니다.`);
+  };
+
+  const onEdit = (targetId, newContent) => {
+    setData(
+      data.map((it) =>
+        it.id === targetId ? { ...it, content: newContent } : it
+      )
+    );
+  };
+
   return (
     <div>
-      <h2>일기장</h2>
-      <DiaryEditor />
+      <DiaryEditor onCreate={onCreate} />
+      <DiaryList diaryList={data} onRemove={onRemove} onEdit={onEdit} />
     </div>
   );
 }
---
+++ src/components/DiaryList.js
@@ -0,0 +1,30 @@
+import React from "react";
+import DiaryItem from "./DiaryItem";
+
+const DiaryList = ({ onEdit, onRemove, diaryList }) => {
+  console.log(diaryList);
+  return (
+    <div className='DiaryList'>
+      <h2>일기 리스트</h2>
+      <h4>{diaryList.length}개의 일기가 있습니다.</h4>
+      <div>
+        {diaryList.map((it) => {
+          return (
+            <DiaryItem
+              key={it.id}
+              {...it}
+              onEdit={onEdit}
+              onRemove={onRemove}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+DiaryList.defaultProp = {
+  diaryList: [],
+};
+
+export default DiaryList;
